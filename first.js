e2ebdecef74ec3127ac70fac992ec67eaac20a38
Nitin Kamal <kamalnitin307@gmail.com>
2026-04-15 12:54:05 +0530

    Second last

--- first.js
+++ first.js
@@ -4,10 +4,7 @@ console.log(button);
 const login = "Login page added";
 console.log(login);
 
-<<<<<<< HEAD
 const footer = "Footer added in our website";
 console.log(footer);
-=======
 const Payment = "Integrated the payment gateway";
 console.log(Payment);
->>>>>>> 67ee482 (Payment added)
